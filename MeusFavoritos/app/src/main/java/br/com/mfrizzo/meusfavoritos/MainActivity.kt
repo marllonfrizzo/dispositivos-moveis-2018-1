b090c762809523f44e170aff11cd692252fc95eb
Marllon Frizzo <mffrizzo@gmail.com>
2018-03-09 21:14:16 -0300

    Activity, Layouts e Views

--- MeusFavoritos/app/src/main/java/br/com/mfrizzo/meusfavoritos/MainActivity.kt
+++ MeusFavoritos/app/src/main/java/br/com/mfrizzo/meusfavoritos/MainActivity.kt
@@ -2,11 +2,17 @@ package br.com.mfrizzo.meusfavoritos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        for (i in Favoritos.getLista()) {
+            lista.append(i+"\n\n\n")
+        }
     }
 }
+
+
