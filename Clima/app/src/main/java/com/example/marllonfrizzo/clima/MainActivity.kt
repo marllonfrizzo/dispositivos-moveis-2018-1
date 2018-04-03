de5e25b1fbd642a3bb7462704fa983c3dfb7984b
Marllon Frizzo <mffrizzo@gmail.com>
2018-04-02 21:14:46 -0300

    Adição de novos arquivos, conexão com a internet e retorno de informação

--- Clima/app/src/main/java/com/example/marllonfrizzo/clima/MainActivity.kt
+++ Clima/app/src/main/java/com/example/marllonfrizzo/clima/MainActivity.kt
@@ -1,61 +1,49 @@
 package com.example.marllonfrizzo.clima
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.marllonfrizzo.clima.util.NetworkUtils
+import com.example.marllonfrizzo.clima.dados.ClimaPreferencias
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
-    val dadosClimaFicticios = listOf("Hoje - Céu limpo - 17°C / 15°C",
-            "Amanhã - Nublado - 19°C / 15°C",
-            "Quinta - Chuvoso - 30°C / 11°C",
-            "Sexta - Chuva com raios - 21°C / 9°C",
-            "Sábado - Chuva com raios - 16°C / 7°C",
-            "Domingo - Chuvoso - 16°C / 8°C",
-            "Segunda - Parcialmente nublado - 15°C / 10°C",
-            "Ter, Mai 24 - Vai curintia - 16°C / 18°C",
-            "Qua, Mai 25 - Nublado - 19°C / 15°C",
-            "Qui, Mai 26 - Tempestade - 30°C / 11°C",
-            "Sex, Mai 27 - Furacão - 21°C / 9°C",
-            "Sáb, Mai 28 - Meteóro - 16°C / 7°C",
-            "Dom, Mai 29 - Apocalipse - 16°C / 8°C",
-            "Seg, Mai 30 - Pós-apocalipse - 15°C / 10°C")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        exJson()
-
-        /* for (clima in dadosClimaFicticios) {
-            dados_clima.append("$clima \n\n\n")
-        } */
+        carregarDadosClima()
     }
 
-    fun exJson() {
-        var dadosJson =
-                """
-            {
-              "temperatura": {
-                "minima": 11.34,
-                "maxima": 19.01
-              },
-              "clima": {
-                "id": 801,
-                "condicao": "Nuvens",
-                "descricao": "poucas nuvens"
-              },
-              "pressao": 1023.51,
-              "umidade": 87
+    fun carregarDadosClima() {
+        val local = ClimaPreferencias.getLocalizacaoSalva(this)
+        val url = NetworkUtils.construirUrl(local)
+        buscarClimaTask().execute(url)
+    }
+
+    inner class buscarClimaTask() : AsyncTask<URL, Void, String>() {
+
+        override fun doInBackground(vararg p0: URL?): String? {
+            try {
+                val url = p0[0]
+                val result = NetworkUtils.obterRespostaDaUrlHttp(url!!)
+
+                return result
+            } catch (ex: Exception) {
+                ex.printStackTrace()
             }
-                """
+            return null
+        }
 
-
-        val objJson = JSONObject(dadosJson)
-        val clima = objJson.getJSONObject("clima")
-        val condicao = clima.getString("condicao")
-        Log.d("exJson", "A condicao e $condicao")
+        override fun onPostExecute(result: String?) {
+            if (result != null && !result.isEmpty()) {
+                dados_clima.text = result
+            }
+        }
     }
+
 }
