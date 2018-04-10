0ce5dd9b06775088bad8d8089e37e9603a8cccba
Marllon Frizzo <mffrizzo@gmail.com>
2018-04-09 21:01:22 -0300

    Respondendo aos cliques dos itens

--- ListaVerde/app/src/main/java/br/com/mfrizzo/listaverde/ListaVerdeAdapter.kt
+++ ListaVerde/app/src/main/java/br/com/mfrizzo/listaverde/ListaVerdeAdapter.kt
@@ -12,10 +12,12 @@ class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolde
     val lista: List<Int>
     val context: Context
     var viewHolderCount = 0
+    val itemClickListener: ItemClickListener
 
-    constructor(context: Context, list: List<Int>) {
+    constructor(context: Context, list: List<Int>, itemClickListener: ItemClickListener) {
         this.lista = list
         this.context = context
+        this.itemClickListener = itemClickListener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumeroViewHolder {
@@ -40,11 +42,29 @@ class ListaVerdeAdapter : RecyclerView.Adapter<ListaVerdeAdapter.NumeroViewHolde
         holder.tvItemNumero?.text = numero.toString()
     }
 
-    inner class NumeroViewHolder : RecyclerView.ViewHolder {
+    interface ItemClickListener {
+        fun onItemClick(position: Int)
+        fun onLongItemClick(position: Int)
+    }
+
+    inner class NumeroViewHolder : RecyclerView.ViewHolder, View.OnClickListener, View.OnLongClickListener {
         val tvItemNumero: TextView?
 
         constructor(itemView: View?) : super(itemView) {
             tvItemNumero = itemView?.findViewById<TextView>(R.id.tv_item_numero)
+            itemView?.setOnClickListener(this)
+            itemView?.setOnLongClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val posicaoClicada = adapterPosition
+            itemClickListener.onItemClick(posicaoClicada)
+        }
+
+        override fun onLongClick(v: View?): Boolean {
+            val posicaoClicada = adapterPosition
+            itemClickListener.onLongItemClick(posicaoClicada)
+            return true
         }
 
     }
