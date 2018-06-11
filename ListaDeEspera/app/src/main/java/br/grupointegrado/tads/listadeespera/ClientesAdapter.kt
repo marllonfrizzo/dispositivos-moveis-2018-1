3e3c2d30097e34be2992e81c8b1b366354b04c84
Marllon Frizzo <mffrizzo@gmail.com>
2018-06-11 20:59:43 -0300

    Inserindo e recuperando clientes do banco de dados

--- ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ClientesAdapter.kt
+++ ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/ClientesAdapter.kt
@@ -1,12 +1,25 @@
 package br.grupointegrado.tads.listadeespera
 
+import android.database.Cursor
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class ClientesAdapter : RecyclerView.Adapter<ClientesAdapter.ClienteViewHolder>() {
+class ClientesAdapter : RecyclerView.Adapter<ClientesAdapter.ClienteViewHolder> {
+
+    private var cursor: Cursor
+
+    constructor(cursor: Cursor) {
+        this.cursor = cursor
+    }
+
+    fun atualizarCursor(cursor: Cursor) {
+        this.cursor.close()
+        this.cursor = cursor
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClienteViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -15,11 +28,22 @@ class ClientesAdapter : RecyclerView.Adapter<ClientesAdapter.ClienteViewHolder>(
     }
 
     override fun onBindViewHolder(holder: ClienteViewHolder, position: Int) {
+        if (!cursor.moveToPosition(position)) {
+            return
+        }
 
+        val colunaNome = cursor.getColumnIndex(ListaEsperaContrato.Clientes.COLUNA_NOME)
+        val nome = cursor.getString(colunaNome)
+
+        val colunaTamanhoGrupo = cursor.getColumnIndex(ListaEsperaContrato.Clientes.COLUNA_TAMANHO_GRUPO)
+        val tamanhoGrupo = cursor.getInt(colunaTamanhoGrupo)
+
+        holder.tvNome.text = nome
+        holder.tvTamanhoGrupo.text = tamanhoGrupo.toString()
     }
 
     override fun getItemCount(): Int {
-        return 0
+        return cursor.count
     }
 
     inner class ClienteViewHolder : RecyclerView.ViewHolder {
--- ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/MainActivity.kt
+++ ListaDeEspera/app/src/main/java/br/grupointegrado/tads/listadeespera/MainActivity.kt
@@ -1,5 +1,8 @@
 package br.grupointegrado.tads.listadeespera
 
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -9,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var clientesAdapter: ClientesAdapter? = null
+    private var database: SQLiteDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +20,58 @@ class MainActivity : AppCompatActivity() {
 
         rv_clientes.layoutManager = LinearLayoutManager(this)
 
-        clientesAdapter = ClientesAdapter()
+        val bdHelper = ListaEsperaBdHelper(this)
+        database = bdHelper.writableDatabase
 
+        // Inserir alguns clientes de exemplo
+        BdUtil.inserirDadosFicticios(database)
+
+        val cursor = getTodosClientes()
+
+        clientesAdapter = ClientesAdapter(cursor)
         rv_clientes.adapter = clientesAdapter
     }
 
-    fun adicionar(view: View) {
+    private fun getTodosClientes() : Cursor {
+        return database!!.query(
+                ListaEsperaContrato.Clientes.TABELA,
+                null,
+                null,
+                null,
+                null,
+                null,
+                ListaEsperaContrato.Clientes.COLUNA_DATA_HORA
+        )
+    }
 
+    fun adicionar(view: View) {
+        if (ed_nome_cliente.text.isEmpty()) {
+            return
+        }
+
+        val nome = ed_nome_cliente.text.toString()
+        var tamanhoGrupo = 1
+
+        try {
+            tamanhoGrupo = ed_tamanho_grupo.text.toString().toInt()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+
+        adicionarNovoCliente(nome, tamanhoGrupo)
+        clientesAdapter!!.atualizarCursor(getTodosClientes())
+
+        ed_nome_cliente.text.clear()
+        ed_tamanho_grupo.text.clear()
+        ed_tamanho_grupo.clearFocus()
+    }
+
+    private fun adicionarNovoCliente(nome: String, tamanhoGrupo: Int): Long {
+        val cliente = ContentValues()
+        cliente.put(ListaEsperaContrato.Clientes.COLUNA_NOME, nome)
+        cliente.put(ListaEsperaContrato.Clientes.COLUNA_TAMANHO_GRUPO, tamanhoGrupo)
+
+        return database!!.insert(ListaEsperaContrato.Clientes.TABELA, null, cliente)
     }
 
 }
