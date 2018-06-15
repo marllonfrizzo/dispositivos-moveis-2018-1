f4d900c77e224076ae7d01550c6ed01c0f54d010
Marllon Frizzo <mffrizzo@gmail.com>
2018-06-15 20:57:38 -0300

    Classe de contrato e helper para banco de dados

---
+++ Clima/app/src/main/java/com/example/marllonfrizzo/clima/ClimaContrato.kt
@@ -0,0 +1,17 @@
+package com.example.marllonfrizzo.clima
+
+import android.provider.BaseColumns
+
+object ClimaContrato {
+    internal object Clima : BaseColumns {
+        const val TABELA = "clima"
+        const val COLUNA_DATA_HORA = "data_hora"
+        const val COLUNA_CLIMA_ID = "clima_id"
+        const val COLUNA_MIN_TEMP = "min_temp"
+        const val COLUNA_MAX_TEMP = "max_temp"
+        const val COLUNA_UMIDADE = "umidade"
+        const val COLUNA_PRESSAO = "pressao"
+        const val COLUNA_VEL_VENTO = "vel_vento"
+        const val COLUNA_GRAUS = "graus"
+    }
+}
